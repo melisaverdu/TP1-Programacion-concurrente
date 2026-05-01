2c868b2095a74f4d100059a14cda1dbe2ae861c1
melisa <melisa.verdu@mi.unc.edu.ar>
2026-05-01 19:11:46 -0300

    feat: enhance job processing in Scheduler with retry logic for node acquisition

--- TrabajoPractico1/src/Scheduler.java
+++ TrabajoPractico1/src/Scheduler.java
@@ -11,7 +11,35 @@ Ejecutado por 3 hilos
 
 */
 public class Scheduler implements Runnable{
-    public void run(){
 
+    JobQueue enCola;
+    JobQueue creados;
+    NodeMatrix matriz;
+
+    public Scheduler(JobQueue enCola, JobQueue creados, NodeMatrix matriz) {
+        this.enCola = enCola;
+        this.creados = creados;
+        this.matriz = matriz;
+    }
+
+    @Override
+    public void run(){
+        Job job = creados.popJob(); // saco un job de los creados
+        if (job != null) { // si hay un job para procesar
+            Node nodo = matriz.ocuparNodoAleatorio(); // intento ocupar un nodo aleatorio
+
+            while (nodo == null) { // si no pude ocupar un nodo, sigo intentando
+                nodo = matriz.ocuparNodoAleatorio();
+                if(nodo == null) {
+                    try {
+                        Thread.sleep(100); // espero un poco antes de intentar de nuevo para evitar un ciclo muy rápido
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt(); // restablecer el estado de interrupción
+                    }
+                }
+            }
+            job.setNodoAsignado(nodo); // asigno el nodo al job
+            enCola.pushJob(job); // agrego el job a la cola de jobs en espera de validacion
+        }
     }
 }
